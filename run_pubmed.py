a1d45adfaed237379a01f418585a99f00f17c31b
Sai Lasya Mynampati <87901791+sailasya-m@users.noreply.github.com>
2025-03-08 20:42:33 +0530

    Update run_pubmed.py

--- run_pubmed.py
+++ run_pubmed.py
@@ -1,5 +1,5 @@
 import argparse
-from punmed_fetcher.punmed_fetcher import PubMedFetcher  # Ensure the correct import
+from punmed_fetcher.punmed_fetcher import PubMedFetcher  
 
 def main():
     parser = argparse.ArgumentParser(description="Fetch PubMed papers with pharma/biotech authors.")
@@ -8,17 +8,17 @@ def main():
 
     args = parser.parse_args()
     
-    # Initialize the fetcher
+
     fetcher = PubMedFetcher(debug=True)
 
-    # Get papers with pharmaceutical/biotech authors
+ 
     papers = fetcher.get_papers_with_pharma_authors(args.query)
 
-    # Export results
+
     fetcher.export_to_csv(papers, args.file)
 
     print(f"Results saved to {args.file}")
 
-# Ensure the script runs only when executed directly
+
 if __name__ == "__main__":
     main()
